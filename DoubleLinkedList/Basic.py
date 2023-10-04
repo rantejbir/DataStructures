cf1dc30874d161be8ddc06462ddfde46e9aa360d
rantejbir <rantejbir@icloud.com>
2023-10-04 00:24:23 -0400

    linkedList

---
+++ DoubleLinkedList/Basic.py
@@ -0,0 +1,36 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.prev = None
+
+
+class DoubleLinkedList:
+    def __init__(self, value):
+        new_node = Node(value)
+        self.head = new_node
+        self.tail = new_node
+        self.length = 1
+
+    def print_list(self):
+        temp = self.head
+        while temp is not None:
+            print(temp.value)
+            temp = temp.next
+
+    def append(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            self.tail.next = new_node
+            new_node.prev = self.tail
+            self.tail = new_node
+        self.length += 1
+        return True
+
+
+mylist = DoubleLinkedList(7)
+mylist.append(23)
+mylist.print_list()

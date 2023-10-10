fb4c3776feb906519058c6cb3f1b5aefe966b94e
rantejbir <rantejbir@icloud.com>
2023-10-10 11:28:48 -0400

    linkedList

--- DoubleLinkedList/Basic.py
+++ DoubleLinkedList/Basic.py
@@ -12,8 +12,29 @@ class DoubleLinkedList:
         self.tail = new_node
         self.length = 1
 
+    def get(self, index):
+        if index < 0 or index >= self.length:
+            return False
+        temp = self.head
+        if index < self.length / 2:
+            for _ in range(index):
+                temp = temp.next
+        else:
+            temp = self.tail
+            for _ in range(self.length - 1, index, -1):
+                temp = temp.prev
+        return temp
+
+    def set_value(self, index, value):
+        temp = self.get(index)
+        if temp:
+            temp.value = value;
+            return True
+        return False
+
     def print_list(self):
         temp = self.head
+        print("--x--x--x--x--x--x--")
         while temp is not None:
             print(temp.value)
             temp = temp.next
@@ -30,7 +51,97 @@ class DoubleLinkedList:
         self.length += 1
         return True
 
+    def prepend(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head.prev = new_node
+            self.head = new_node
+        self.length += 1
+        return True
+
+    def insert(self, index, value):
+        if index < 0 or index >= self.length:
+            return False
+        if index == self.length:
+            self.append(value)
+        if index == 0:
+            self.prepend(value)
+        else:
+            new_node = Node(value)
+            before = self.get(index - 1)
+            after = before.next
+
+            new_node.prev = before
+            new_node.next = after
+
+            before.next = new_node
+            after.prev = new_node
+        self.length += 1
+        return True
+
+    def remove(self, index):
+        if index < 0 or index >= self.length:
+            return False
+        if index == self.length:
+            self.pop()
+        if index == 0:
+            self.pop_first()
+        else:
+            temp = self.get(index)
+            temp.prev.next = temp.next
+            temp.next.prev = temp.prev
+            temp.next = None
+            temp.prev = None
+        self.length -= 1
+        return True
+
+    def pop(self):
+        temp = self.tail
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        if self.head is None:
+            return None
+        else:
+            self.tail = self.tail.prev
+            self.tail.next = None
+            temp.prev = None
+        self.length -= 1
+        return temp
+
+    def pop_first(self):
+        temp = self.head
+        if self.length == 1:
+            self.head = None
+            self.tail = None
+        if self.head is None:
+            return None
+        else:
+            self.head = self.head.next
+            self.head.prev = None
+            temp.next = None
+        self.length -= 1
+        return temp
+
 
 mylist = DoubleLinkedList(7)
 mylist.append(23)
+mylist.append(25)
+mylist.append(56)
+mylist.print_list()
+# mylist.prepend(0)
+
+mylist.print_list()
+# mylist.pop_first()
+mylist.print_list()
+print((mylist.get(1)).value)
+mylist.insert(1, 404)
+mylist.print_list()
+mylist.insert(4, 404)
+mylist.print_list()
+mylist.remove(4)
 mylist.print_list()
